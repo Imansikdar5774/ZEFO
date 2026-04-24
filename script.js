d36c0599b44ab417cac8786a31a037d15000a62d
Imansikdar5774 <sikdarmithu93@gmail.com>
2026-04-24 23:33:44 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -9,7 +9,17 @@ let outputBox = document.getElementById("output-box");
 
 runBtn.addEventListener('click',() => {
   // code naam ka variable jaha value hoga editor
-  let code = editor.value;
-  // output box me dikhayega text ka contain editor ka
-  outputBox.textContent = code;
-})
+  let code = editor.value.trim();
+
+  // agar code bik se suru hota hei to tab output me value ata hei
+  if (code.startsWith('bik')) {
+    outputBox.textContent = code.substring(4).trim();
+    // isme iska color hoga black
+  }
+  // agar ulta sidha code ho to error feek ke marega
+  else {
+    outputBox.textContent = "sintax error: invalid sintax";
+    // error ka color hei red
+    outputBox.style.color = "red";
+  }
+})  
